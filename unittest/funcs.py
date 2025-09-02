b9c1024dcf0ce9f75cfe57262bfadaee3f45e27d
JJhun <jungjihun926@gmail.com>
2025-09-02 13:10:53 +0900

    실습2

---
+++ unittest/funcs.py
@@ -0,0 +1,14 @@
+def is_even(value=10):
+    if value%2==0:
+        return True
+    elif value%2==1:
+        return False
+    
+def calculate_average(list):
+    return sum(list)/len(list)
+
+def max_list(list):
+    return max(list)
+
+def min_list(list):
+    return min(list)
--- unittest/test_funcs.py
+++ unittest/test_funcs.py
@@ -1,6 +1,19 @@
-# TODO: 사용자 모듈 import
+from funcs import is_even, calculate_average, max_list, min_list 
 
+def test_is_even():
+    assert True == is_even(10)
+    assert False == is_even(25)
+    assert True == is_even(3434)
 
-# TODO: 아래의 코드를 삭제하고 unittest를 작성하세요.
-def test_always():
-    assert False, "Remove me!"
+def test_calculate_average():
+    assert 3 == calculate_average([1,2,3,4,5])
+    assert 0 == calculate_average([-2,-1,0,1,2])
+
+def test_max_list():
+    assert 10 == max_list([0,2,4,6,8,10])
+    assert 10.1 == max_list([-3.5, 1, 10.1, 2, 9, 8.5])
+
+def test_min_list():
+    assert -15 == min_list([-10, -9, 1, -15, 11])
+    assert 5 == min_list([30, 62, 12, 5, 32])
+
